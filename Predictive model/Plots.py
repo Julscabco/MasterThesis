1296664194e69d3206d3696c8c5a9a2d5be500ab
Julscabco <pht915@alumni.ku.dk>
2023-11-10 16:19:41 +0100

    Organixed code and results for monday 13 of November meeting in Simulation 11 folder

--- Predictive model/Plots.py
+++ Predictive model/Plots.py
@@ -19,177 +19,196 @@ import matplotlib.pyplot as plt
 from Utils import nutrient_integral
 
 
+simulation_id = 11
+folder_name = 'Simulation'+'_'+str(simulation_id)
+storage_directory = os.path.join(os.getcwd(),os.path.join('RESULTS',folder_name))
 
-file_name = '0.01_500_1000000.0_0.0_10000000.0_10000.csv'
-storage_directory = 'RESULTS'
+destiny_directory = os.path.join(os.getcwd(),os.path.join('FIGURES',folder_name))
+if not os.path.exists(destiny_directory):
+    os.mkdir(destiny_directory,0o666)
+   
 
+""" ----- PLOT 1 CONFIG ----- """
 # Plot of number of phage, bacteria or nutrients vs time
 plot_1 = True
-parameters_in_title_1 = ['delta_t','Nb0']
-parameters_symbols_1 = [r'$\Delta t=$', r'${N}_{b0}=$']
+save_plot_1 = True
 
+parameters_in_title_1 = ['Nb0','B','P','n0']
+parameters_symbols_1 = [r'${Nb}_{0}$', r'${B}_{0}=$',r'${P}_{0}=$',r'${N}_{0}=$']
+
+
+
+
+""" ------- PLOT 2 CONFIG ------ """
 # Plot of bacteria and nutrients vs time compared to theoretical integration
-plot_2 = True
+plot_2 = False
+save_plot_2 = False
+
+parameters_in_title_2 = ['B','n0']
+parameters_symbols_2 = [r'${B}_{0}$',r'${N}_{0}$']
+
+
 
 """ ------- FILE READING --------"""
 
-complete_path = os.path.join(os.getcwd(),os.path.join(storage_directory,file_name))
+for filename in os.listdir(storage_directory):
+    complete_path = os.path.join(storage_directory,filename)
+    ncomb = (filename.split('_')[-1]).split('.')[0]
 
-with open(complete_path,"r") as csvfile:
-    csvreader = csv.reader(csvfile)
+    with open(complete_path,"r") as csvfile:
+        csvreader = csv.reader(csvfile)
     
-    for row in csvreader:
-        if 'PARAMETERS' in row:
+        for row in csvreader:
+            if 'PARAMETERS' in row:
+                break
+    
+        for row in csvreader:
+            column_names = row
             break
     
-    for row in csvreader:
-        column_names = row
-        break
-    
-    df_parameters= pd.DataFrame(columns=column_names)
-    i = 0
-    for row in csvreader:
-        if len(row)>2:
-            df_parameters.loc[i] = row
-            i = i + 1
-        else:
-            break
+        df_parameters= pd.DataFrame(columns=column_names)
+        i = 0
+        for row in csvreader:
+            if len(row)>2:
+                df_parameters.loc[i] = row
+                i = i + 1
+            else:
+                break
 
-    for row in csvreader:
-        if 'ADDITIONAL DATA FROM SIMULATION' in row:
-            break
+        for row in csvreader:
+            if 'ADDITIONAL DATA FROM SIMULATION' in row:
+                break
         
-    for row in csvreader:
-         column_names = row
-         break
-        
-    df_add_parameters = pd.DataFrame(columns = column_names)
-    
-    i = 0
-    for row in csvreader:
-        if len(row)>2:
-            df_add_parameters.loc[i] = row
-            i = i + 1
-        else:
-            break
-    
-    
-    time = []
-    nphages = []
-    nbacteria = []
-    nnutrients = []
-    
-
-    for row in csvreader:
-        if '---' in row:
+        for row in csvreader:
+            column_names = row
             break
         
-    for row in csvreader:
-        t,p,b,n = row
-        time.append(float(t))
-        nphages.append(float(p))
-        nbacteria.append(float(b))
-        nnutrients.append(float(n))
-
-time = np.array(time)
-nphages = np.array(nphages)
-nbacteria = np.array(nbacteria)
-nnutrients = np.array(nnutrients)
-
-"""---------PLOT 1: PHAGE, BACTERIA AND NUTRIENTS VS TIME --------"""
-
-if plot_1 == True  :
+        df_add_parameters = pd.DataFrame(columns = column_names)
     
-    fig, ax = plt.subplots(3, 1, figsize=(10, 10))
-    
-    title = ''
-    for i in range(0,len(parameters_in_title_1)):
-        title += str(parameters_symbols_1[i])
-        title += str((df_parameters['Value'][df_parameters['Name in code']==parameters_in_title_1[i]])[i])
-        title += ', '
-
-    ax[0].plot(time, nphages, label='Phage')
-    ax[0].set_ylabel('Number of phage')
-    ax[0].set_title(title)
-
-    ax[0].set_xlim(0.0,time[-1])
-
-    ax[1].plot(time, nbacteria, label='Bacteria', color='orange')
-
-
-    ax[1].set_ylabel('Number of bacteria')
-
-    ax[1].set_xlim(0.0,time[-1])
-
-    ax[2].plot(time, nnutrients)
-    ax[2].set_xlabel('Time (h)',fontsize=20)
-    ax[2].set_ylabel('Number of nutrients')
-    ax[2].set_xlim(0.0,time[-1])
-    
-
+        i = 0
+        for row in csvreader:
+            if len(row)>2:
+                df_add_parameters.loc[i] = row
+                i = i + 1
+            else:
+                break
     
     
+        time = []
+        nphages = []
+        nbacteria = []
+        nnutrients = []
+    
+
+        for row in csvreader:
+            if '---' in row:
+                break
+        
+        for row in csvreader:
+            t,p,b,n = row
+            time.append(float(t))
+            nphages.append(float(p))
+            nbacteria.append(float(b))
+            nnutrients.append(float(n))
+
+    time = np.array(time)
+    nphages = np.array(nphages)
+    nbacteria = np.array(nbacteria)
+    nnutrients = np.array(nnutrients)
+
+    """---------PLOT 1: PHAGE, BACTERIA AND NUTRIENTS VS TIME --------"""
+
+    if plot_1 == True  :
+    
+        fig, ax = plt.subplots(3, 1, figsize=(10, 10))
+    
+        title = ''
+        for i in range(0,len(parameters_in_title_1)):
+            title += str(parameters_symbols_1[i])
+            title += str(np.array(df_parameters['Value'][df_parameters['Name in code']==parameters_in_title_1[i]])[0])
+            title += ', '
+
+        ax[0].plot(time, nphages, label='Phage')
+        ax[0].set_ylabel('Number of phage',fontsize=16)
+        ax[0].set_title(title)
+
+        ax[0].set_xlim(0.0,time[-1])
+
+        ax[1].plot(time, nbacteria, label='Bacteria', color='orange')
+
+
+        ax[1].set_ylabel('Number of bacteria',fontsize=16)
+
+        ax[1].set_xlim(0.0,time[-1])
+
+        ax[2].plot(time, nnutrients)
+        ax[2].set_xlabel('Time (h)',fontsize=20)
+        ax[2].set_ylabel('Number of nutrients',fontsize=16)
+        ax[2].set_xlim(0.0,time[-1])
+        
+        for axis in ax:
+            axis.tick_params(axis='y', labelsize=14)
+    
+        if save_plot_1 == True:
+            figure_name = 'P1_S'+str(simulation_id)+'_comb_'+str(ncomb)+'.png'
+            fig.savefig(os.path.join(destiny_directory, figure_name))
+        
+            print("Plot 1 saved as:",figure_name)
+    
+
+    
 
 
 
 
-""" PLOT 2: BACTERIA AND NUTRIENTS VS TIME (SIMULATION VS INTEGRATION COMPARISON) """
 
-if plot_2 == True:
+    """ PLOT 2: BACTERIA AND NUTRIENTS VS TIME (SIMULATION VS INTEGRATION COMPARISON) """
+
+    if plot_2 == True:
     
     
-    B = float(df_parameters.at[(df_parameters['Name in code']=='B').idxmax(),'Value'])
-    n0 = float(df_parameters.at[(df_parameters['Name in code']=='n0').idxmax(),'Value'])
-    V = float(df_add_parameters.at[(df_add_parameters['Magnitude']=='Volume of the system').idxmax(),'Value'])
+        B = float(df_parameters.at[(df_parameters['Name in code']=='B').idxmax(),'Value'])
+        n0 = float(df_parameters.at[(df_parameters['Name in code']=='n0').idxmax(),'Value'])
+        V = float(df_add_parameters.at[(df_add_parameters['Magnitude']=='Volume of the system').idxmax(),'Value'])
     
-    # We integrate the differential equations that the bacteria and virus should follow
-    t,nutrients,bacteria = nutrient_integral(0.034,float(B),float(n0),(time[1]-time[0])*60.0,time[-1]*60.0)
+        # We integrate the differential equations that the bacteria and virus should follow
+        t,nutrients,bacteria = nutrient_integral(0.034,float(B),float(n0),(time[1]-time[0])*60.0,time[-1]*60.0)
     
-    t = [j/60.0 for j in t]
-    nbacteria_c = [j/V for j in nbacteria]
-    nnutrients_c = [j/V for j in nnutrients]
+        t = [j/60.0 for j in t]
+        nbacteria_c = [j/V for j in nbacteria]
+        nnutrients_c = [j/V for j in nnutrients]
+    
+        title = ''
+        for i in range(0,len(parameters_in_title_2)):
+            title += (str(parameters_symbols_2[i]) + '=')
+            title += str(np.array(df_parameters['Value'][df_parameters['Name in code']==parameters_in_title_2[i]])[0])
+            title += ', '
     
     
-    fig2, ax2 = plt.subplots()
-    ax2.plot(t, bacteria, label = 'B(t)',color='black',linestyle='dashed')
-    ax2.plot(time, nbacteria_c, label='Simulation',color = 'black')
-    ax2.set_xlabel('time (hours)')
-    ax2.set_ylabel('Bacteria concentration (ml-1)')
-    ax2.legend(loc='best')
-
-
-    ax2.plot(t, nutrients, label = 'n(t)',color = 'red',linestyle='dashed')
-    ax2.plot(time, nnutrients_c, label='Simulation', color = 'red')
-    ax2.set_xlabel('time (mins)')
-    ax2.set_ylabel('Bacteria concentration (ml-1)')
-    ax2.legend(loc='best')
+        fig2, ax2 = plt.subplots()
+    
+        ax2.plot(t, bacteria, label = 'B(t)',color='black',linestyle='dashed')
+        ax2.plot(time, nbacteria_c, label='Simulation',color = 'black')
+        ax2.plot(t, nutrients, label = 'n(t)',color = 'red',linestyle='dashed')
+        ax2.plot(time, nnutrients_c, label='Simulation', color = 'red')
+    
+        ax2.set_xlabel('time (mins)')
+        ax2.set_ylabel('Concentration (ml-1)')
+        ax2.set_title(title)
+        ax2.legend(loc='best')
+    
+        if save_plot_2 == True:
+            figure_name = 'P2_S'+str(simulation_id)+'_comb_'+str(ncomb)+'.png'
+            fig2.savefig(os.path.join(destiny_directory, figure_name))
+        
+            print("Plot 2 saved as:",figure_name)
     
         
     
     
-        
-    
-    
-    
-    
-    
-    
     
 
 
-
-
-
-save_plot = False
-
-
-if save_plot == True:
-    figure_path = os.path.join(os.getcwd(), 'FIGURES', 'First figures')
-    figure_name = 'NLG_' + str(delta_t) + '_' + str(niter) + '_' + str(Nb0)+ '.png'
-    fig.savefig(os.path.join(figure_path, figure_name))
-
-
-    
     
 
 
@@ -197,6 +216,27 @@ if save_plot == True:
 
 
 
-plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- Predictive model/Simulation_runner.py
+++ Predictive model/Simulation_runner.py
@@ -7,6 +7,7 @@ Created on Thu Nov  9 13:43:07 2023
 
 import os
 import csv
+import itertools
 
 import time as pytime
 
@@ -18,86 +19,109 @@ from Predictive_model import system_evolution_nlg
 """ ------- PARAMETERS ------- """
 
 delta_t = 0.01
-Nb0 = 500
-B = 1.0e6
-P = 0.0
+values_Nb0 = [1000]
+values_B = [1.0e6]
+values_P = [1.0e3,1.0e4,1.0e5,1.0e6,1.0e7,1.0e8]
 N = 60
 k = 6.0
 nd = 30
 nu = 5.0e-10
-n0 = 1.0e7
+values_n0 = [1.0e7,1.0e6,1.0e7]
 
 # The doubling time in minutes
 doubling_time_mins = 20.0
 doubling_time = minutes_to_taul(doubling_time_mins, N, k)
 
+""" ---------- WE FETCH THE NUMBER OF SIMULATION -------"""
+
+with open('simulation_number.txt', 'r') as file:
+    counter = int(file.read())
+
+# Increment the counter
+counter += 1
+
+with open('simulation_number.txt', 'w') as file:
+    file.write(str(counter))
+    
+storage_directory = 'Simulation_'+str(counter)
+
+folder_path = os.path.join(os.getcwd(),'RESULTS',storage_directory)
+if not os.path.exists(folder_path):
+    os.mkdir(folder_path,0o666)
+
 
 """------------ NUMBA COMPILATION -------------"""
 # We call the function with just one iteration so that numba does the compilation
 niter = 1
-_, _, _, _ = system_evolution_nlg(delta_t, Nb0, B, P, N, k, nd, nu, n0, doubling_time, niter)
+_, _, _, _ = system_evolution_nlg(delta_t, values_Nb0[0], values_B[0], values_P[0], N, k, nd, nu, values_n0[0], doubling_time, niter)
 
 print('Numba compilation done')
 
 
 """ ---------- ACTUAL SIMULATION ------------- """
 niter = 10000
-
 start = pytime.time()
-time_vector, nphages, nbacteria, nnutrients = system_evolution_nlg(delta_t, Nb0, B, P, N, k, nd, nu, n0, doubling_time, niter)
-total_time = pytime.time()-start
+
+param_combinations = list(itertools.product(values_Nb0,values_B, values_P, values_n0))
+ncomb = 0
+
+for combination in param_combinations:
+    
+    Nb0,B,P,n0 = combination
+    print(P)
+
+    time_vector, nphages, nbacteria, nnutrients = system_evolution_nlg(delta_t, Nb0, B, P, N, k, nd, nu, n0, doubling_time, niter)
+    total_time = pytime.time()-start
+    
+
+    time_hours = [taul_to_minutes(t, N, k)/60.0 for t in time_vector]
+
+
+    """ --------- STORAGE OF THE DATA IN A FILE --------- """
+
+    file_name = 'Simulation'+'_'+str(counter)+'_Combination_'+str(ncomb)+'.csv'
+    complete_path = os.path.join(folder_path,file_name)
+
+    with open(complete_path,'w',newline='') as csvfile:
+    
+        f = csv.writer(csvfile,delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    
+
+        f.writerow(["PARAMETERS"])
+        f.writerow(['Parameter','Name in code', 'Value','Units'])
+        f.writerow(['Delta t','delta_t',delta_t, 'tau_l units'])
+        f.writerow(['Initial number of bacteria','Nb0',Nb0, '-'])
+        f.writerow(['Initial concentration of bacteria','B',B, 'cells/ml'])
+        f.writerow(['Initial concentration of phage', 'P',P, 'cells/ml'])
+        f.writerow(['Maximum number of timer molecules','N', N, '-'])
+        f.writerow(['Production rate of timer molecules','k', k, 'cells/min·ml'])
+        f.writerow(['Number of dead timer molecules when superinfected','nd', nd, '-'])
+        f.writerow(['Adsorption rate of phage to bacteria','nu', nu, 'cells/min·ml'])
+        f.writerow(['Initial concentration of nutrients','n0', n0, 'nutrient/ml'])
+
+        f.writerow([])
+        f.writerow([])
+
+        f.writerow(['ADDITIONAL DATA FROM SIMULATION'])
+        f.writerow(['Magnitude', 'Value','Unit'])
+        f.writerow(['Duration', total_time, 's' ])
+        f.writerow(['Volume of the system', float(Nb0)/float(B), 'ml'])
+        f.writerow(['Number of iterations',niter,'-'])
+
+        f.writerow([])
+        f.writerow([])
+
+        f.writerow(['DATA FROM SIMULATION'])
+        f.writerow(['Time (hours)','Number of phage','Number of bacteria', 'Number of nutrients'])
+        f.writerow(['---'])
+    
+        for i in range(0,niter):
+            f.writerow([time_hours[i],nphages[i],nbacteria[i],nnutrients[i]])
+
+        ncomb += 1
+        
 print('Code lasted in', total_time, 'seconds')
-
-time_hours = [taul_to_minutes(t, N, k)/60.0 for t in time_vector]
-
-
-
-
-""" --------- STORAGE OF THE DATA IN A FILE --------- """
-
-file_name = str(delta_t) + '_' + str(Nb0) + '_' + str(B) + '_' + str(P) + '_' + str(n0) + '_'+ str(niter) + '.csv'
-
-storage_directory = 'RESULTS'
-complete_path = os.path.join(os.getcwd(),storage_directory,file_name)
-
-with open(complete_path,'w',newline='') as csvfile:
-    
-    f = csv.writer(csvfile,delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    
-
-    f.writerow(["PARAMETERS"])
-    f.writerow(['Parameter','Name in code', 'Value','Units'])
-    f.writerow(['Delta t','delta_t',delta_t, 'tau_l units'])
-    f.writerow(['Initial number of bacteria','Nb0',Nb0, '-'])
-    f.writerow(['Initial concentration of bacteria','B',B, 'cells/ml'])
-    f.writerow(['Initial concentration of phage', 'P',P, 'cells/ml'])
-    f.writerow(['Maximum number of timer molecules','N', N, '-'])
-    f.writerow(['Production rate of timer molecules','k', k, 'cells/min·ml'])
-    f.writerow(['Number of dead timer molecules when superinfected','nd', nd, '-'])
-    f.writerow(['Adsorption rate of phage to bacteria','nu', nu, 'cells/min·ml'])
-    f.writerow(['Initial concentration of nutrients','n0', n0, 'nutrient/ml'])
-
-    f.writerow([])
-    f.writerow([])
-
-    f.writerow(['ADDITIONAL DATA FROM SIMULATION'])
-    f.writerow(['Magnitude', 'Value','Unit'])
-    f.writerow(['Duration', total_time, 's' ])
-    f.writerow(['Volume of the system', float(Nb0)/float(B), 'ml'])
-    f.writerow(['Number of iterations',niter,'-'])
-
-    f.writerow([])
-    f.writerow([])
-
-    f.writerow(['DATA FROM SIMULATION'])
-    f.writerow(['Time (hours)','Number of phage','Number of bacteria', 'Number of nutrients'])
-    f.writerow(['---'])
-    
-    for i in range(0,niter):
-        f.writerow([time_hours[i],nphages[i],nbacteria[i],nnutrients[i]])
-
-
-
+print('Simulation id:',counter)
 
 
 
